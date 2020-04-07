dde4a3fbe118bebf7dcd5807121d427037f9ac4b
awp974310 <50905863+awp974310@users.noreply.github.com>
2020-04-07 21:36:18 +0800

    預設值、方法多載

--- 3D Game/Assets/Scripts/Car.cs
+++ 3D Game/Assets/Scripts/Car.cs
@@ -4,8 +4,30 @@ public class Car : MonoBehaviour
 {
     //方法語法：修飾詞 傳回類型 方法名稱(參數) {  }
     //void 無傳回：使用此方法不會傳回任何值
-    public void Drive()
+    //參數語法：類型 名稱
+    /// <summary>
+    /// 開車，直達幼兒園
+    /// </summary>
+    public void Drive(float speed, string sound = "嘟嘟嘟",string dir ="幼兒園")
     {
-        print("沒時間解釋了，快上車！");
+        print("沒時間解釋了，快上車！ " + sound);
+        print("本班車直達幼兒園，時速 " + speed + "公里");
+        print("行徑目標：" + dir);
+    }
+
+    public bool Aircontrol()
+    {
+        print("冷氣開起來");
+        return true;
+    }
+
+    public void Fly()
+    {
+        print("車子飛高高");
+    }
+
+    public void Fly(int height)
+    {
+        print("車子飛很高，目前高度：" + height + "公尺");
     }
 }
--- 3D Game/Assets/Scripts/CarManager.cs
+++ 3D Game/Assets/Scripts/CarManager.cs
@@ -6,6 +6,13 @@ public class CarManager : MonoBehaviour
 
     public void Start()
     {
-        car1.Drive();
+        car1.Drive(30);
+        car1.Drive(70, dir:"去幼兒園的路上");
+        car1.Drive(119.9f,"叭~~ 碰! 啪! 框! 磅(翻車聲)","已失去目標");
+
+        print("一台快車是否有開冷氣？" + car1.Aircontrol());
+
+        car1.Fly();
+        car1.Fly(200);
     }
 }
