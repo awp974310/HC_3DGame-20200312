f00b008d48c1ea8ab246e2f00a239554af71edd3
awp974310 <50905863+awp974310@users.noreply.github.com>
2020-04-30 21:38:02 +0800

    新增怪物、關卡資料

---
+++ 3D Game/Assets/Scripts/SpwanManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpwanManager : MonoBehaviour
+{
+    [Header("生成資料")]
+    public SpawnData data;
+
+    private void Start()
+    {
+        StartCoroutine(StartSpawn());
+    }
+
+    private IEnumerator StartSpawn()
+    {
+        for(int i = 0; i < data.spawn.Length; i++)
+        {
+            yield return new WaitForSeconds(data.spawn[i].time);
+            print(data.spawn[i].name);
+        }
+    }
+}
